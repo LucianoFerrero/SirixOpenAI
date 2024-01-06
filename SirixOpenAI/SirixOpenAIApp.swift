4ffa8fd432feb93c030ebffb3cc37d1fff9a24fd
Luciano Ferrero <122234485+LucianoFerrero@users.noreply.github.com>
2024-01-06 15:29:57 -0300

    Initial Commit

---
+++ SirixOpenAI/SirixOpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SirixOpenAIApp.swift
+//  SirixOpenAI
+//
+//  Created by Luciano Ferrero on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SirixOpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
